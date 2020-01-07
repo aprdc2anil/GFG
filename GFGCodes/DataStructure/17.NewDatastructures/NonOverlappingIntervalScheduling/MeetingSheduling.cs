4b23452d962e734b1504db05c9ff127ac5ac53a1
Anilkumar Ramisetty <aprdc2anil@gmail.com>
2020-01-07 07:30:26 +0530

    NonOverlapping Interval Scheduling High Level

---
+++ GFGCodes/DataStructure/17.NewDatastructures/NonOverlappingIntervalScheduling/MeetingSheduling.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GFGCodes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Interval
+    {
+        public int Low { get; set; }
+        public int High { get; set; }
+
+        public Interval(int low, int high)
+        {
+            if (high <= low)
+            {
+                throw new InvalidOperationException("'high' should be higher than 'low'.");
+            }
+
+            this.Low = low;
+            this.High = high;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BinarySearchIntervalTreeNode
+    {
+        public BinarySearchIntervalTreeNode LeftNode { get; set; }
+        public BinarySearchIntervalTreeNode RightNode { get; set; }
+        public Interval NodeInterval { get; set; }      
+       
+        public BinarySearchIntervalTreeNode(Interval interval)
+        {
+            this.NodeInterval = interval;
+            LeftNode = RightNode = null;
+        }
+    }
+
+    public class BinarySearchIntervalTree
+    {
+        private BinarySearchIntervalTreeNode root;
+
+        // to do
+        public bool Add(Interval interval)
+        {
+            if (root == null)
+            {
+                root = new BinarySearchIntervalTreeNode(interval);
+                return true;
+            }
+            else
+            {
+               return PrivateAdd(root, interval);
+            }
+        }
+
+        private bool PrivateAdd(BinarySearchIntervalTreeNode root, Interval interval)
+        {
+            if (interval.High <= root.NodeInterval.Low)
+            {
+                if (root.LeftNode == null)
+                {
+                    root.LeftNode = new BinarySearchIntervalTreeNode(interval);
+                    return true;
+                }
+                else
+                {
+                   return PrivateAdd(root.LeftNode, interval);
+                }
+            }
+            else if (interval.Low >= root.NodeInterval.High)
+            {
+                if (root.RightNode == null)
+                {
+                    root.RightNode = new BinarySearchIntervalTreeNode(interval);
+                    return true;
+                }
+                else
+                {
+                    return PrivateAdd(root.RightNode, interval);
+                }
+            }
+            else
+            {
+                return false;
+                /*     root 10 , 20
+                //     interval low less than 20 
+                //          or
+                //     interval high is greater than 10
+                // 7 9|10 goes to left
+                // 11, 25 overlap with root
+                // 11, 19 overlap with root
+                // 8,15 overlap with root
+                // 20|21, 25 goes to right
+                */
+            }
+        }
+
+        public bool CheckAvailability(Interval interval)
+        {
+            if (root == null)
+            {
+                root = new BinarySearchIntervalTreeNode(interval);
+                return true;
+            }
+            return PrivateCheckAvailability(root, interval);
+        }
+
+        private bool PrivateCheckAvailability(BinarySearchIntervalTreeNode root, Interval interval)
+        {
+            if (interval.High <= root.NodeInterval.Low)
+            {
+                if (root.LeftNode == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return PrivateCheckAvailability(root.LeftNode, interval);
+                }
+            }
+            else if (interval.Low >= root.NodeInterval.High)
+            {
+                if (root.RightNode == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return PrivateCheckAvailability(root.RightNode, interval);
+                }
+            }
+            else
+            {
+                return false;
+                /*     root 10 , 20
+                //     interval low less than 20 
+                //          or
+                //     interval high is greater than 10
+                // 7 9|10 goes to left
+                // 11, 25 overlap with root
+                // 11, 19 overlap with root
+                // 8,15 overlap with root
+                // 20|21, 25 goes to right
+                */
+            }
+        }
+
+       // to do , similar to bst deletion
+        public bool Delete()
+        {
+            return false;
+        }
+
+    }
+
+    class MeetingSheduling
+    {
+
+    }
+}
--- GFGCodes/Program.cs
+++ GFGCodes/Program.cs
@@ -9,35 +9,7 @@ namespace GFGCodes
     {
         static void Main(string[] args)
         {
-            Dictionary<int, int> x = new Dictionary<int, int>();
-
-            for (int i = 0; i < 5; ++i)
-            {
-                x.Add(i, 0);
-            }
-
-
-            Console.WriteLine(" befor increment {0}", string.Join(" ", x.Values));
-
-            for (int i = 0; i < 5; ++i)
-            {
-                x[i]++;
-            }
-
-            Console.WriteLine(" after increment {0}", string.Join(" ", x.Values));
-
-            // RotateMatrix.EntryPoint();
-            Console.ReadLine();            
-        }
-
-        public static void EntryPoint()
-        {
-
-        }
-
-        public static void Algorithm()
-        {
-
+            LargestAreaHistogram.EntryPoint();
         }
     }
 }
